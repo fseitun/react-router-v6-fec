3e8f81608cbd8c37491f721865014a6858b28f8d
joxpulp <joxpulp@gmail.com>
2021-09-28 20:32:32 -0500

    feat: add useLocation example

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function About() {
-  return <h1>About</h1>;
+	return (
+		<>
+			<h1>About</h1>
+			<p style={{ fontSize: '100px' }}>
+				lorem loremlorem loremlorem lorem lorem loremlorem loremlorem loremlorem
+				loremlorem lorem lorem loremlorem loremlorem lorem lorem loremlorem
+				lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+				lorem lorem lorem lorem lorem lorem loremlorem loremlorem loremlorem
+				lorem lorem loremlorem loremlorem lorem lorem loremlorem loremlorem
+				loremlorem loremlorem lorem lorem loremlorem loremlorem lorem lorem
+				loremlorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+				lorem lorem lorem lorem lorem lorem lorem lorem loremlorem loremlorem
+				loremlorem lorem lorem loremlorem loremlorem lorem lorem loremlorem
+				loremlorem loremlorem loremlorem lorem lorem loremlorem loremlorem lorem
+			</p>
+			<Link to='/'>Ir al inicio</Link>
+		</>
+	);
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 export default function Home() {
-  return <h1>Home</h1>;
+	const location = useLocation();
+
+	useEffect(() => {
+		window.scrollTo({ top: 0 });
+	}, [location]);
+
+	return (
+		<motion.div initial={{ opacity: 0, y: -100 }} animate={{ opacity: 1, y: 0 }}>
+			<h1>Home</h1>
+			<p style={{ fontSize: '100px' }}>
+				lorem loremlorem loremlorem lorem lorem loremlorem loremlorem loremlorem
+				loremlorem lorem lorem loremlorem loremlorem lorem lorem loremlorem
+				lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+				lorem lorem lorem lorem lorem lorem loremlorem loremlorem loremlorem
+				lorem lorem loremlorem loremlorem lorem lorem loremlorem loremlorem
+				loremlorem loremlorem lorem lorem loremlorem loremlorem lorem lorem
+				loremlorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+				lorem lorem lorem lorem lorem lorem lorem lorem loremlorem loremlorem
+				loremlorem lorem lorem loremlorem loremlorem lorem lorem loremlorem
+				loremlorem loremlorem loremlorem lorem lorem loremlorem loremlorem lorem
+			</p>
+		</motion.div>
+	);
 }
